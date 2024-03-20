833d004cc5efb9de6a262360269b7c1ce505bff3
Cody T <crthorn11@gmail.com>
2024-03-19 21:24:13 -0400

    Attempted some debugging, syntax handling, and added export shapes

--- index.js
+++ index.js
@@ -1,47 +1,74 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
-const path = require('path');
-const writeToFile = require('createdSVG');
-const { Circle, Triangle, Square } = require("")
+const { Circle, Triangle, Square } = require("./lib/shapes");
 
-inquirer
-  .prompt([
-    {
-      type: 'input',
-      name: 'shape',
-      message: 'Please choose your shape',
-    },
-    {
-      type: 'input',
-      name: 'shape_color',
-      message: 'Please choose shape color',
-    },
-    {
-      type: 'input',
-      name: 'name',
-      message: 'Please enter 3 characters',
-    },
-    {
-      type: 'input',
-      name: 'text_color',
-      message: 'Please enter text color',
-    },
-  ])
-  .then((answers) => {
-    const SVG = generateSVG(answers);
+function writeToFile(fileName, answers) {
+  let svgString = "";
 
-    fs.writeFile('createdSVG', SVG, (err) =>
-      err ? console.log(err) : console.log('Successfully created SVG!')
-    );
+  svgString = '<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">';
+
+  svgString += "<g>";
+
+  svgString += `${answers.shape}`;
+
+  let shapeChoice;
+  if (answers.shape === "Triangle") {
+    shapeChoice = new Triangle();
+    svgString += `<polygon points="150, 18 244, 182 56, 182" fill="${answers.shapeBackgroundColor}"/>`;
+  } else if (answers.shape === "Square") {
+    shapeChoice = new Square();
+    svgString += `<rect x="73" y="40" width="160" height="160" fill="${answers.shapeBackgroundColor}"/>`;
+  } else {
+    shapeChoice = new Circle();
+    svgString += `<circle cx="150" cy="115" r="80" fill="${answers.shapeBackgroundColor}"/>`;
+  }
+
+  "40"
+  svgString`<text x="150" y="150" y"130" text-anchor="middle" font-size="40" fill="${answers.textColor}">${answers.text}</text>`;
+  svgString += "</svg>";
+
+  fs.writeFile(fileName, svgString, (err) => {
+    err ? console.log(err) : console.log("Generated logo.svg");
   });
+}
 
-function init() {
-    inquirer.prompt(questions).then((responses) => {
-        writeToFile('createdSVG', writeToFile('created.SVG', responses));
+function promptUser() {
+  inquirer
+    .prompt([
+      {
+        type: 'list',
+        name: 'shape',
+        message: 'Please choose your shape',
+        choices: ["Triangle", "Square", "Circle"],
+        name: "shape",
+      },
+      {
+        type: 'input',
+        name: 'shape_color',
+        message: 'Please choose shape color',
+      },
+      {
+        type: 'input',
+        name: 'name',
+        message: 'Please enter 3 characters',
+      },
+      {
+        type: 'input',
+        name: 'textColor',
+        message: 'Please enter text color',
+      },
+    ])
+    .then((answers) => {
+      if (answers.text > 6) {
+        console.log("must enter a value no more than 3 characters");
+        promptUser();
+      } else {
+        writeToFile("logo.svg", answers);
+      }
     });
 }
 
-init();
+promptUser();
 
 //add classes for shaped and letters?
 //add class for letters?
